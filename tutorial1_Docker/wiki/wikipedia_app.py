92c1feea00253ffa95dfdae487353a43e978c08d
Gostatsog <gustavomedina512@gmail.com>
2021-01-01 23:09:39 -0300

    tutorial 2 y 3

---
+++ tutorial1_Docker/wiki/wikipedia_app.py
@@ -0,0 +1,5 @@
+import os 
+import wikipod 
+
+#Buscar dato
+wikiPod.buscarDato()
---
+++ tutorial1_Docker/wiki/wikipod.py
@@ -0,0 +1,30 @@
+import wikipedia
+import pageviewapi
+import pymongo
+import os
+
+DATABASE = "wiki"
+COLLECTION = "wikicol"               
+
+class wikiPod:
+     #Constructor
+      def __init__(self,channel):
+         self.channel = channel
+
+     #subir informacion
+      def subir_mensaje(dato):
+         #conexion a MONGO
+         myclient = pymongo.MongoClient(host=os.environ['MONGO_HOST'], port=int(os.environ['MONGO_PORT']))
+         db = client[DATABASE]
+         col = db[COLLECTION]
+         col.insert_one({title:dato.title,content:dato.content})
+
+     #Buscar dato
+      def buscarDato(dat="Programming"):
+         dat = input("¿Qué articulo desea buscar?: ")        
+         a = wikipedia.search(dat)
+         for i in a:
+            print(i, " ")
+         datr = input("¿Cual de estos resultados en específico desea guardar?(recuerde el uso de mayusculas y minusculas): ")
+         w = wikipedia.page(datr)
+         wikiPod.subir_mensaje(w)
