7da39ec778f419731a97a6d8ef65ddcd41c28a58
Gostatsog <gustavomedina512@gmail.com>
2021-01-06 15:09:12 -0300

    md tut1 tut2

--- tutorial2_RabbitMQ/WikiConsumer.py
+++ tutorial2_RabbitMQ/WikiConsumer.py
@@ -27,7 +27,6 @@ channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
 result = channel.queue_declare(queue='', exclusive=True)
 queue_name = result.method.queue
 
-#for severity in severities:
 channel.queue_bind(exchange='direct_logs', queue=queue_name, routing_key='Wiki')
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
